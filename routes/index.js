46751f9fbf1aa2dc4f28f60535a32027fb246359
Ken Duigraha Putra <email": null>
2016-10-30 17:46:38 +0700

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,34 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/indexController')
 
-/* GET home page. */
+/* show home page. */
 router.get('/', controller.viewHome);
+
+/* show signup form. */
+router.get('/signup', controller.viewSignUp);
+
+/* process signup page. */
+router.post('/signup', controller.processSignUp);
+
+/* show login form. */
+router.get('/login', controller.viewLogIn);
+
+/* process login page. */
+router.post('/login', controller.processLogin);
+
+/* show add new data form. */
+router.get('/add', controller.viewAddData);
+
+/* process add new data form. */
+router.post('/add', controller.processAddData);
+
+/* show edit a data form. */
+router.get('/edit/:id', controller.viewEditData);
+
+/* process edit a data form. */
+router.post('/edit', controller.processEditData);
+
+/* process delete data form. */
+router.get('/delete/:id', controller.processDelete);
 
 module.exports = router;
